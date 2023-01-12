5781ba5274ab6e1e47d967b20a61c7f452c2e37f
Eric Pan <erzpan@gmail.com>
2023-01-12 11:01:11 -0800

    Add material design to make it look nicer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgChartsModule } from 'ng2-charts';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatButtonModule } from '@angular/material/button'
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -16,7 +21,13 @@ import { NgChartsModule } from 'ng2-charts';
     HttpClientModule,
     FormsModule,
     AppRoutingModule,
-    NgChartsModule
+    NgChartsModule,
+    BrowserAnimationsModule,
+    
+    // Material Design modules
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
